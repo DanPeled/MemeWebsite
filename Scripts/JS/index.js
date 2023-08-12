3929be3b5be7d8e0d1ebcb2f86aa4a6fe100f860
DanPeled <danpeled31@gmail.com>
2023-08-12 10:59:09 +0300

    removed uneccessary code

--- Scripts/JS/index.js
+++ Scripts/JS/index.js
@@ -51,31 +51,11 @@ async function loadRandomMeme() {
 }
 async function loadMemeButtonFunc() {
     let text;
-    let id = getMemeID();
+    let id = prompt("Please Enter Meme ID:", "0");
     if (id == null || id == "") {
         return;
     } else {
         loadMeme(parseInt(id))
-    }
-}
-function getMemeID() {
-    while (true) {
-        const userInput = prompt("Please Enter Meme ID:");
-
-        // Check if the user clicked "Cancel" or entered an empty string
-        if (userInput === null || userInput === "") {
-            return null;
-        }
-
-        // Convert the user input to a number
-        const numberValue = parseFloat(userInput);
-
-        // Check if the conversion was successful and it's a valid number
-        if (!isNaN(numberValue)) {
-            return numberValue;
-        } else {
-            alert("Invalid input. Please enter a valid number.");
-        }
     }
 }
 async function loadMeme(memeIndex) {
