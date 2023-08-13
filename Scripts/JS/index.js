340e617b6b37a7216d287bd8c55a0b336d0d4413
DanPeled <danpeled31@gmail.com>
2023-08-13 09:57:30 +0300

    made the site faster, removed a file mistaken

--- Scripts/JS/index.js
+++ Scripts/JS/index.js
@@ -1,115 +1,103 @@
-let previosmeme = 0;
 document.getElementById('loadRandommemesButton').addEventListener('click', loadRandomMeme);
-document.getElementById("loadMemeButton").addEventListener('click', loadMemeButtonFunc)
-screen.orientation.lock("portrait");
+document.getElementById('loadMemeButton').addEventListener('click', loadMemeButtonFunc);
+screen.orientation.lock('portrait');
+
+const memesImage = document.getElementById('memesImage');
+const memesVideo = document.getElementById('memesVideo');
+const memeIdText = document.getElementById('memeIdText');
+const downloadButton = document.getElementById('downloadButton');
+const memesContainer = document.getElementById('memesContainer');
+
+let previosmeme = 0;
+
+async function fetchMemesList() {
+    const response = await fetch('memes/meme-list.json');
+    const memesList = await response.json();
+    return memesList;
+}
+
+function displayMeme(memeFilename) {
+    const isImage = /\.(jpg|png|webp|PNG)$/i.test(memeFilename);
+    const isVideo = /\.(mp4|webm|mov|webmp)$/i.test(memeFilename);
+
+    if (isImage) {
+        memesImage.src = `memes/${memeFilename}`;
+        memesImage.style.display = 'block';
+        memesVideo.style.display = 'none';
+    } else if (isVideo) {
+        memesVideo.src = `memes/${memeFilename}`;
+        memesVideo.style.display = 'block';
+        memesImage.style.display = 'none';
+    } else {
+        console.error('Unsupported meme format:', memeFilename);
+    }
+}
+
 async function loadRandomMeme() {
     try {
-        const memesImage = document.getElementById('memesImage');
-        const memesVideo = document.getElementById('memesVideo');
-        memesVideo.currentTime = 0;
+        const memesList = await fetchMemesList();
         memesVideo.pause();
         memesVideo.src = '';
-        const response = await fetch('memes/meme-list.json'); // Fetch the list of meme filenames
-        const memesList = await response.json();
 
         if (memesList.length === 0) {
             alert('No memes available.');
             return;
         }
 
-        // Choose a random meme filename
         const randomIndex = Math.floor(Math.random() * memesList.length);
-        document.getElementById('memeIdText').textContent = `Meme ID: ${randomIndex}`;
+        memeIdText.textContent = `Meme ID: ${randomIndex}`;
         const randomMemeFilename = memesList[randomIndex];
 
         previosmeme = randomIndex;
-        // Display the meme (image or video)
-        const memesContainer = document.getElementById('memesContainer');
-
-
-        // Update the download button's href attribute
-        const downloadButton = document.getElementById('downloadButton');
         downloadButton.href = `memes/${randomMemeFilename}`;
         downloadButton.setAttribute('download', randomMemeFilename);
+        displayMeme(randomMemeFilename);
 
-        // Determine whether the random meme is an image or video
-        if (randomMemeFilename.endsWith('.jpg') || randomMemeFilename.endsWith('.png') || randomMemeFilename.endsWith('.webp')) {
-            memesImage.src = `memes/${randomMemeFilename}`;
-            memesImage.style.display = 'block';
-            memesVideo.style.display = 'none';
-        } else if (randomMemeFilename.endsWith('.mp4') || randomMemeFilename.endsWith('.webm') || randomMemeFilename.endsWith('.mov') || randomMemeFilename.endsWith('.webmp')) {
-            memesVideo.src = `memes/${randomMemeFilename}`;
-            memesVideo.style.display = 'block';
-            memesImage.style.display = 'none';
-        } else {
-            console.error('Unsupported meme format:', randomMemeFilename);
-            return;
-        }
     } catch (error) {
         console.error('Error loading memes:', error);
     }
 }
+
 async function loadMemeButtonFunc() {
-    let text;
-    let id = prompt("Please Enter Meme ID:", "0");
-    if (id == null || id == "") {
+    const id = prompt('Please Enter Meme ID:', '0');
+
+    if (!id) {
         return;
-    } else {
-        loadMeme(parseInt(id))
+    }
+
+    const memeIndex = parseInt(id);
+    if (!isNaN(memeIndex)) {
+        loadMeme(memeIndex);
     }
 }
+
 async function loadMeme(memeIndex) {
     try {
-
-        const memesImage = document.getElementById('memesImage');
-        const memesVideo = document.getElementById('memesVideo');
-        memesVideo.currentTime = 0;
+        const memesList = await fetchMemesList();
         memesVideo.pause();
-        memesVideo.setAttribute('src', null);
-
-        const response = await fetch('memes/meme-list.json'); // Fetch the list of meme filenames
-        const memesList = await response.json();
+        memesVideo.src = '';
 
         if (memesList.length === 0) {
             alert('No memes available.');
             return;
         }
+
         if (memeIndex >= memesList.length || memeIndex < 0) {
-            document.getElementById('memeIdText').innerHTML = `Meme ID: <span style="color: red; font-weight: bold;">INVALID ID</span>`;
+            memeIdText.innerHTML = `Meme ID: <span style="color: red; font-weight: bold;">INVALID ID</span>`;
             memesImage.src = 'internalimages/missingmeme.jpg';
             return;
         }
-        // Choose a random meme filename
+
         const randomMemeFilename = memesList[memeIndex];
-        document.getElementById('memeIdText').textContent = `Meme ID: ${memeIndex}`;
+        memeIdText.textContent = `Meme ID: ${memeIndex}`;
         previosmeme = memeIndex;
-        // Display the meme (image or video)
-        const memesContainer = document.getElementById('memesContainer');
-
-
-        // Update the download button's href attribute
-        const downloadButton = document.getElementById('downloadButton');
         downloadButton.href = `memes/${randomMemeFilename}`;
         downloadButton.setAttribute('download', randomMemeFilename);
-
-        // Determine whether the random meme is an image or video
-        if (randomMemeFilename.endsWith('.jpg') || randomMemeFilename.endsWith('.png') || randomMemeFilename.endsWith('.webp')) {
-            memesImage.src = `memes/${randomMemeFilename}`;
-            memesImage.style.display = 'block';
-            memesVideo.style.display = 'none';
-        } else if (randomMemeFilename.endsWith('.mp4') || randomMemeFilename.endsWith('.webm') || randomMemeFilename.endsWith('.mov') || randomMemeFilename.endsWith('.webmp')) {
-            memesVideo.src = `memes/${randomMemeFilename}`;
-            memesVideo.style.display = 'block';
-            memesImage.style.display = 'none';
-        } else {
-            console.error('Unsupported meme format:', randomMemeFilename);
-            return;
-        }
+        displayMeme(randomMemeFilename);
 
     } catch (error) {
         console.error('Error loading memes:', error);
     }
 }
-
 
 loadRandomMeme();
